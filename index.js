cb44a2fd01d7486d287efad2d0b74466cc0bf1fe
Brian Richter <brianorwhatever@gmail.com>
2016-03-08 15:36:53 -0800

    trying without quotes

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ function* update(next) {
       throw new Error("The todo must have a field `id`.");
     }
 
-    var result = yield r.table('todos').filter({'ip': this.request.headers["x-forwarded-for"]}, {'id': todo.id}).update(todo, {returnChanges: "always"}).run(this._rdbConn);
+    var result = yield r.table('todos').filter({ip: this.request.headers["x-forwarded-for"]}, {id: todo.id}).update(todo, {returnChanges: "always"}).run(this._rdbConn);
     this.body = JSON.stringify(result.changes[0].new_val);
   }
   catch(e) {
@@ -108,7 +108,7 @@ function* del(next) {
         throw new Error("The todo must have a field `id`.");
     }
 
-    var result = yield r.table('todos').filter({'ip': this.request.headers["x-forwarded-for"]}, {'id': todo.id}).delete().run(this._rdbConn);
+    var result = yield r.table('todos').filter({ip: this.request.headers["x-forwarded-for"]}, {id: todo.id}).delete().run(this._rdbConn);
     this.body = JSON.stringify({success: true});
   }
   catch(e) {
